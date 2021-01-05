6229f5aea040b1f4403f17eba7b0d0fc07b02bff
Taklemariam Alazar <teklealazar399@gmail.com>
2021-01-05 12:21:49 +0300

    Counting Sort2 solved

---
+++ Week 1/Day 5/05-Counting Sort2.py
@@ -0,0 +1,22 @@
+'''
+Question -> https://www.hackerrank.com/challenges/countingsort2/problem
+
+@Author : Taklemariam Alazar
+Counting Sort Implementation 2
+'''
+def countingSort(arr):
+    maximum = 0
+    for i in range(len(arr)):
+        if arr[i] > maximum:
+            maximum = arr[i]
+    placeHolder = [0]*(maximum + 1)
+    
+    for i in range(len(arr)):
+        placeHolder[arr[i]] += 1
+
+    result = []
+    for i in range(len(placeHolder)):
+        if placeHolder[i] != 0:
+            for j in range(placeHolder[i]):
+                result.append(i)
+    return result
