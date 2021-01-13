5ecc0bc7aaf0d189a6cc013e44858322de5e036a
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2021-01-14 00:14:13 +0300

    Update 02-Recursive Fibonaci.py

--- Week 2/Day 1/02-Recursive Fibonaci.py
+++ Week 2/Day 1/02-Recursive Fibonaci.py
@@ -1,16 +1,5 @@
-# class Solution:
-#     def fib(self, n: int) -> int:
-#         if n <= 1:
-#             return n
-#         else:
-#             return (self.fib((n-2)) + self.fib((n-1)))
-
-def fib(x):
-    if x <= 1:
-        return x
-    
-    for i in range(x):
-        
-print(fib(5))
-        
-
+def fib(self, n: int) -> int:
+    if n <= 1:
+        return n
+    else:
+        return (self.fib((n-2)) + self.fib((n-1)))
