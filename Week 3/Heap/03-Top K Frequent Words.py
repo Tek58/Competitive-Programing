0d8cdb2cfe1d477c6bd096a593e704ea8c875401
Taklemariam Alazar <teklealazar399@gmail.com>
2021-02-04 00:36:37 +0300

    heap 03

---
+++ Week 3/Heap/03-Top K Frequent Words.py
@@ -0,0 +1,22 @@
+'''
+Question -> https://leetcode.com/problems/top-k-frequent-words/
+@Author -> Taklemariam Alazar
+'''
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        dic = {}
+        for i in words:
+            if i in dic:
+                dic[i] += 1
+            else:
+                dic[i] = 1
+        
+        heap = []
+        for i in dic:
+            heapq.heappush(heap, (-dic[i], i))
+        
+        result = []
+        for i in range(k):
+            result.append(heapq.heappop(heap)[1])
+        
+        return result
